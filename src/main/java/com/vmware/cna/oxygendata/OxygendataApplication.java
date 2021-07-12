e63652715a2a630610c9737e1afce14f1a52b52a
pkhamdee <pkhamdee@gmail.com>
2021-07-12 09:54:56 +0700

    change

---
+++ src/main/java/com/vmware/cna/oxygendata/OxygendataApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.cna.oxygendata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxygendataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxygendataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmware/cna/oxygendata/OxygendataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.cna.oxygendata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxygendataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
