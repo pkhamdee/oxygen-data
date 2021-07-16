337ffe18066f602b4299d89b34f577ba74de4fe3
pkhamdee <pkhamdee@gmail.com>
2021-07-16 14:16:01 +0700

    add mapping device user

--- src/main/java/com/vmware/cna/oxygendata/service/DeviceService.java
+++ src/main/java/com/vmware/cna/oxygendata/service/DeviceService.java
@@ -16,7 +16,7 @@ public class DeviceService {
     private UserRepository userRepository;
 
     @Autowired
-    public DeviceService(DeviceRepository deviceRepository, UserRepository userRepository){
+    public DeviceService(DeviceRepository deviceRepository, UserRepository userRepository) {
         this.deviceRepository = deviceRepository;
         this.userRepository = userRepository;
     }
@@ -38,8 +38,10 @@ public class DeviceService {
     }
 
     @Transactional
-    public Device updateDevice(Long id, Device device){
-        if(deviceRepository.findById(id).isPresent()){
+    public Device updateDevice(Long id, Device device) {
+
+        if (deviceRepository.findById(id).isPresent()) {
+
             Device findDevice = deviceRepository.findById(id).get();
             findDevice.setBarcode(device.getBarcode());
             findDevice.setName(device.getName());
@@ -47,34 +49,70 @@ public class DeviceService {
             findDevice.setStatus(device.getStatus());
             findDevice.setStatusDate(device.getStatusDate());
             findDevice.setType(device.getType());
-            
-            if(userRepository.findById(findDevice.getUser().getId()).isPresent()) {
-                User findUser = userRepository.findById(findDevice.getUser().getId()).get();
 
-                findUser.setAddress(findDevice.getUser().getAddress());
-                findUser.setFirstName(findDevice.getUser().getFirstName());
-                findUser.setGender(findDevice.getUser().getGender());
-                findUser.setLastName(findDevice.getUser().getLastName());
-                findUser.setLocation(findDevice.getUser().getLocation());
-                findUser.setPasswd(findDevice.getUser().getPasswd());
-                findUser.setPhone(findDevice.getUser().getPhone());
-                findUser.setServiceDate(findDevice.getUser().getServiceDate());
-                findUser.setSeverity(findDevice.getUser().getSeverity());
-                findUser.setServiceRequestDate(findDevice.getUser().getServiceRequestDate());
-                findUser.setStatusDate(findDevice.getUser().getStatusDate());
-                findUser.setStatus(findDevice.getUser().getStatus());
-                findUser.setType(findDevice.getUser().getType());
-                findUser.setUserName(findDevice.getUser().getUserName());
+            User saveUser = null;
 
-                User saveUser = userRepository.save(findUser);
-                findDevice.setUser(saveUser);
+            if (device.getUser() != null && device.getUser().getId() != null) { // found user, request is to map device with user
+
+                if (userRepository.findById(device.getUser().getId()).isPresent()) {
+
+                    User existUser = userRepository.findById(device.getUser().getId()).get();
+
+                    User newUser = new User();
+                    newUser.setId(existUser.getId());
+                    newUser.setAddress(existUser.getAddress());
+                    newUser.setFirstName(existUser.getFirstName());
+                    newUser.setGender(existUser.getGender());
+                    newUser.setLastName(existUser.getLastName());
+                    newUser.setLocation(existUser.getLocation());
+                    newUser.setPasswd(existUser.getPasswd());
+                    newUser.setPhone(existUser.getPhone());
+                    newUser.setServiceDate(existUser.getServiceDate());
+                    newUser.setSeverity(existUser.getSeverity());
+                    newUser.setServiceRequestDate(existUser.getServiceRequestDate());
+                    newUser.setStatusDate(existUser.getStatusDate());
+                    newUser.setStatus(existUser.getStatus());
+                    newUser.setType(existUser.getType());
+                    newUser.setUserName(existUser.getUserName());
+
+                    saveUser = userRepository.save(newUser);
+                    findDevice.setUser(saveUser);
+
+                } else { // notfound user
+                    throw new ResourceNotFoundException("The specified User id does not found");
+                }
+
+            } else { // get from database
+
+                if (findDevice.getUser() != null && userRepository.findById(findDevice.getUser().getId()).isPresent()) {
+
+                    User findUser = userRepository.findById(findDevice.getUser().getId()).get();
+
+                    findUser.setAddress(findDevice.getUser().getAddress());
+                    findUser.setFirstName(findDevice.getUser().getFirstName());
+                    findUser.setGender(findDevice.getUser().getGender());
+                    findUser.setLastName(findDevice.getUser().getLastName());
+                    findUser.setLocation(findDevice.getUser().getLocation());
+                    findUser.setPasswd(findDevice.getUser().getPasswd());
+                    findUser.setPhone(findDevice.getUser().getPhone());
+                    findUser.setServiceDate(findDevice.getUser().getServiceDate());
+                    findUser.setSeverity(findDevice.getUser().getSeverity());
+                    findUser.setServiceRequestDate(findDevice.getUser().getServiceRequestDate());
+                    findUser.setStatusDate(findDevice.getUser().getStatusDate());
+                    findUser.setStatus(findDevice.getUser().getStatus());
+                    findUser.setType(findDevice.getUser().getType());
+                    findUser.setUserName(findDevice.getUser().getUserName());
+
+                    saveUser = userRepository.save(findUser);
+                    findDevice.setUser(saveUser);
+                }
             }
 
             return deviceRepository.save(findDevice);
-            
 
         } else {
             throw new ResourceNotFoundException("The specified Device is not found ");
         }
+
     }
 }
